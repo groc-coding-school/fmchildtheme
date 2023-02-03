d1dff4f96c0059888386a278d3837e6a7a19e54f
groc-coding-school <groc.coders.schools@gmail.com>
2023-02-03 19:29:14 +0300

    New Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fmchildtheme
+### A theme created by (Groc Coding School)[https://groc-coders.blogspot.com]
+### Using oceanwp theme
---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ * Load the parent style.css file
+ */
+function fruits_child_enqueue_parent_style() {
+
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update the theme).
+	$theme   = wp_get_theme( 'OceanWP' );
+	$version = $theme->get( 'Version' );
+
+	// Load the stylesheet.
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'fruits_child_enqueue_parent_style' );
